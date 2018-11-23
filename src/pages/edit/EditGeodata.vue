8cc0e1d63980c3ced15b90234bed736a820e4b0d
Jonathan Smith <jonathan@peoplesized.com>
2018-11-23 11:31:10 +0200

    start

--- src/pages/edit/EditGeodata.vue
+++ src/pages/edit/EditGeodata.vue
@@ -1,24 +1,49 @@
 <template>
+  <!--INSTRUCTIONS -->
   <div>
-    All you can do here is upload some geodata. Once you're done, you can view and see these files on the <a
-      href="/edit/json">edit</a> page.
-
-    1. Select file
-    2. (Disabled initially) Optionally edit filename
-    3. Upload to server (need to leave this page open?)
+    <h2>Select a geodata file to upload</h2>
+    <p>Geodata should be a JSON file containing a GeoJSON FeatureCollection.</p>
+    <p>It's a good idea to make sure they are already named with the type of geodata and a date or version: e.g. <code>villages.2018-11-23.json</code> </p>
+    <p>This will help you selecting the right file in the <router-link to="/edit/json">config editor</router-link>.</p>
   </div>
+
+  <ul>
+    <li v-for="file in files">{{file.name}} - Error: {{file.error}}, Success: {{file.success}}</li>
+  </ul>
+  <file-upload
+      ref="upload"
+      v-model="files"
+      post-action="/post.method"
+      put-action="/put.method"
+      @input-file="inputFile"
+      @input-filter="inputFilter"
+  >
+    Upload file
+  </file-upload>
+  <button v-show="!$refs.upload || !$refs.upload.active" @click.prevent="$refs.upload.active = true" type="button">Start upload</button>
+  <button v-show="$refs.upload && $refs.upload.active" @click.prevent="$refs.upload.active = false" type="button">Stop upload</button>
+
 </template>
 
 <script lang='ts'>
   import Vue from 'vue';
+  import VueUploadComponent from 'vue-upload-component';
+  Vue.component('file-upload', VueUploadComponent);
 
   export default Vue.extend({
-    mounted() {
-    },
     data() {
-      return {};
+      return {
+        files: null as Array,
+      }
     },
-    methods: {},
+    methods: {
+      handleFileUpload() {
+        console.log('ready to upload something');
+      },
+      submitFile() {
+        console.log('upload some files');
+      },
+    },
   });
 </script>
 
