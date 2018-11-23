0cea0d1303858d8ec2d416122849fa63398d1c97
Jonathan Smith <jonathan@peoplesized.com>
2018-11-23 08:49:17 +0200

    basic text

--- src/pages/edit/EditGeodata.vue
+++ src/pages/edit/EditGeodata.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Edit geodata</div>
+  <div>
+    All you can do here is upload some geodata. Once you're done, you can view and see these files on the <a
+      href="/edit/json">edit</a> page.
+
+    1. Select file
+    2. (Disabled initially) Optionally edit filename
+    3. Upload to server (need to leave this page open?)
+  </div>
 </template>
 
 <script lang='ts'>
