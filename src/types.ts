2c23f290987f9ebcb0af89f379d933c3d1eb8c0a
Jonathan Smith <jonathan@peoplesized.com>
2018-11-25 14:42:25 +0200

    let’s not combine types here

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,21 @@
 import { TConfig } from '@locational/config-validation/build/module/lib/config_types/TConfig';
 import { DoumaUser} from '@locational/douma-types';
+import {TApplets} from '@locational/config-validation/build/module/lib/config_types/TApplets';
 
 export interface Instance {
   _id: string;
   name: string;
 }
 
-interface BasicUser {
-    _id?:string;
-    name:string;
-    username:string;
-}
-
 export interface Permission {
-    _id?:string;
-    user_id:string;
-    instance_id?:string;
-    value:string;
+    _id?: string;
+    user_id: string;
+    instance_id?: string;
+    value: string;
 }
 
+export type Applets = TApplets;
 
 export type InstanceConfig = TConfig;
-export type User = BasicUser | DoumaUser;
+export type DoumaUser = DoumaUser;
+
