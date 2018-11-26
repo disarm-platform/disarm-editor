c180d9c16f423e51e73b8cc2c859f55148adac88
Jonathan Smith <jonathan@peoplesized.com>
2018-11-26 07:14:24 +0200

    rough new user works

--- src/pages/edit/EditUsers.vue
+++ src/pages/edit/EditUsers.vue
@@ -12,6 +12,9 @@
           fixed
           label="Name"
           width="100">
+        <template slot-scope="scope">
+          <span @click="edit_name(scope)">{{scope.row[scope.column.property]}}</span>
+        </template>
       </el-table-column>
       <el-table-column
           label="All | None">
@@ -29,22 +32,25 @@
           with="500"
       >
         <template slot="header" slot-scope="scope">
-          <div>{{scope.column.label}}</div>
+          <div>{{scope.column.property}}</div>
           <el-button type="text" @click="set_all_for_permission(scope)">All</el-button>
           |
           <el-button type="text" @click="set_none_for_permission(scope)">None</el-button>
         </template>
         <template slot-scope="scope">
           <span
-            @click="toggle(scope)"
+              @click="toggle(scope)"
           >
-            <i v-if="scope.row.permissions[scope.column.label]" class="el-icon-success green"></i>
+            <i v-if="scope.row.permissions[scope.column.property]" class="el-icon-success green"></i>
             <i v-else class="el-icon-circle-plus-outline"></i>
           </span>
         </template>
       </el-table-column>
       s
     </el-table>
+
+    <el-button @click='add_user'>add user</el-button>
+
   </div>
 </template>
 
@@ -94,11 +100,11 @@
     },
     methods: {
       isChecked(scope) {
-        return scope.row.permissions[scope.column.label];
+        return scope.row.permissions[scope.column.property];
       },
       toggle(scope: object) {
         const user_id = scope.row._id;
-        const permission = scope.column.label;
+        const permission = scope.column.property;
         toggle_permission(scope, this.permissions, user_id, permission);
       },
       set_all_for_user(scope: object) {
@@ -110,14 +116,25 @@
         this.permissions = bulk_set_all_permissions_for_user(this.permissions, user_id, false, this.permission_options);
       },
       set_all_for_permission(scope: object) {
-        const permission_string = scope.column.label;
+        const permission_string = scope.column.property;
         const value = true;
         this.permissions = bulk_set_permission_for_all_users(this.permissions, this.users, permission_string, value);
       },
       set_none_for_permission(scope: object) {
-        const permission_string = scope.column.label;
+        const permission_string = scope.column.property;
         const value = false;
         this.permissions = bulk_set_permission_for_all_users(this.permissions, this.users, permission_string, value);
+      },
+      add_user() {
+        const new_user = {
+          name: 'No-one',
+          username: 'No name',
+          _id: Math.random(),
+        } as DevBasicUser;
+        this.users.push(new_user);
+      },
+      edit_name(scope) {
+        console.log('edit_name for', scope.row[scope.column.property])
       },
       save() {
         console.log(this.permissions)
