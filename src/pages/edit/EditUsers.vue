500cfcd30ca6fab5f79c00351c09ca66c7af47fe
Jonathan Smith <jonathan@peoplesized.com>
2018-11-26 07:20:21 +0200

    fix lint

--- src/pages/edit/EditUsers.vue
+++ src/pages/edit/EditUsers.vue
@@ -55,96 +55,129 @@
 </template>
 
 <script lang='ts'>
-  import Vue from 'vue';
-  import {DevBasicUser, InstanceConfig, Permission, DevUserWithPermissions} from '@/types';
+import Vue from 'vue';
+import {
+  DevBasicUser,
+  InstanceConfig,
+  Permission,
+  DevUserWithPermissions,
+} from '@/types';
 
-  // DEV/DEBUG ONLY
-  import {sample_users, sample_permissions, sample_applets} from './sampleUsers';
-  import {
-    bulk_set_permission_for_all_users,
-    bulk_set_all_permissions_for_user, toggle_permission,
-    users_and_permissions_for_table
-  } from "@/lib/users_with_permissions"
+// DEV/DEBUG ONLY
+import {
+  sample_users,
+  sample_permissions,
+  sample_applets,
+} from './sampleUsers';
+import {
+  bulk_set_permission_for_all_users,
+  bulk_set_all_permissions_for_user,
+  toggle_permission,
+  users_and_permissions_for_table,
+} from '@/lib/users_with_permissions';
 
+export default Vue.extend({
+  props: {
+    live_instance_config: Object as () => InstanceConfig,
+  },
+  data() {
+    return {
+      users: sample_users as DevBasicUser[],
+      permissions: sample_permissions as Permission[],
+    };
+  },
+  computed: {
+    permission_options(): string[] {
+      const output: string[] = [];
 
-  export default Vue.extend({
-    props: {
-      live_instance_config: Object as () => InstanceConfig,
-    },
-    data() {
-      return {
-        users: sample_users as DevBasicUser[],
-        permissions: sample_permissions as Permission[],
-      };
-    },
-    computed: {
-      permission_options(): string[] {
-        const output = [];
+      const types = ['write', 'read'];
+      // const applet_names: string[] = Object.keys(this.live_instance_config.applets);
+      const applet_names: string[] = Object.keys(sample_applets);
 
-        const types = ['write', 'read'];
-        // const applet_names: string[] = Object.keys(this.live_instance_config.applets);
-        const applet_names: string[] = Object.keys(sample_applets);
-
-        applet_names.forEach((applet) => {
-          types.forEach((type) => {
-            output.push(`${type}:${applet}`);
-          });
+      applet_names.forEach((applet) => {
+        types.forEach((type) => {
+          output.push(`${type}:${applet}`);
         });
-        return output;
-      },
-      users_with_permissions(): DevUserWithPermissions[] {
-        return users_and_permissions_for_table(this.users, this.permissions, this.permission_options)
-      },
+      });
+      return output;
     },
-    mounted() {
+    users_with_permissions(): DevUserWithPermissions[] {
+      return users_and_permissions_for_table(
+        this.users,
+        this.permissions,
+        this.permission_options,
+      );
     },
-    methods: {
-      isChecked(scope) {
-        return scope.row.permissions[scope.column.property];
-      },
-      toggle(scope: object) {
-        const user_id = scope.row._id;
-        const permission = scope.column.property;
-        toggle_permission(scope, this.permissions, user_id, permission);
-      },
-      set_all_for_user(scope: object) {
-        const user_id = scope.row._id;
-        this.permissions = bulk_set_all_permissions_for_user(this.permissions, user_id, true, this.permission_options);
-      },
-      set_none_for_user(scope: object) {
-        const user_id = scope.row._id;
-        this.permissions = bulk_set_all_permissions_for_user(this.permissions, user_id, false, this.permission_options);
-      },
-      set_all_for_permission(scope: object) {
-        const permission_string = scope.column.property;
-        const value = true;
-        this.permissions = bulk_set_permission_for_all_users(this.permissions, this.users, permission_string, value);
-      },
-      set_none_for_permission(scope: object) {
-        const permission_string = scope.column.property;
-        const value = false;
-        this.permissions = bulk_set_permission_for_all_users(this.permissions, this.users, permission_string, value);
-      },
-      add_user() {
-        const new_user = {
-          name: 'No-one',
-          username: 'No name',
-          _id: Math.random(),
-        } as DevBasicUser;
-        this.users.push(new_user);
-      },
-      edit_name(scope) {
-        console.log('edit_name for', scope.row[scope.column.property])
-      },
-      save() {
-        console.log(this.permissions)
-      },
+  },
+  methods: {
+    isChecked(scope: any) {
+      return scope.row.permissions[scope.column.property];
     },
-  });
+    toggle(scope: any) {
+      const user_id = scope.row._id;
+      const permission = scope.column.property;
+      toggle_permission(scope, this.permissions, user_id, permission);
+    },
+    set_all_for_user(scope: any) {
+      const user_id = scope.row._id;
+      this.permissions = bulk_set_all_permissions_for_user(
+        this.permissions,
+        user_id,
+        true,
+        this.permission_options,
+      );
+    },
+    set_none_for_user(scope: any) {
+      const user_id = scope.row._id;
+      this.permissions = bulk_set_all_permissions_for_user(
+        this.permissions,
+        user_id,
+        false,
+        this.permission_options,
+      );
+    },
+    set_all_for_permission(scope: any) {
+      const permission_string = scope.column.property;
+      const value = true;
+      this.permissions = bulk_set_permission_for_all_users(
+        this.permissions,
+        this.users,
+        permission_string,
+        value,
+      );
+    },
+    set_none_for_permission(scope: any) {
+      const permission_string = scope.column.property;
+      const value = false;
+      this.permissions = bulk_set_permission_for_all_users(
+        this.permissions,
+        this.users,
+        permission_string,
+        value,
+      );
+    },
+    add_user() {
+      const new_user = {
+        name: 'No-one',
+        username: 'No name',
+        _id: `${Math.random()}`,
+      } as DevBasicUser;
+
+      console.log('Using Math.random() for user._id');
+      this.users.push(new_user);
+    },
+    edit_name(scope: any) {
+      console.log('edit_name for', scope.row[scope.column.property]);
+    },
+    save() {
+      console.log(this.permissions);
+    },
+  },
+});
 </script>
 
 <style lang='scss' scoped>
-  .green {
-    color: #2cd02c;
-  }
+.green {
+  color: #2cd02c;
+}
 </style>
