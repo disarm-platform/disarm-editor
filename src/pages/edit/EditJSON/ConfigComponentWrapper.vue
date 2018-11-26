58a436313c2ce8129f27196c05c36d78480f9f30
Jonathan Smith <jonathan@peoplesized.com>
2018-11-26 15:20:40 +0200

    plenty commented

--- src/pages/edit/EditJSON/ConfigComponentWrapper.vue
+++ src/pages/edit/EditJSON/ConfigComponentWrapper.vue
@@ -9,7 +9,7 @@
       <!--</div>-->
     </div>
 
-    <ComponentMessages :validation_result="validation_result" :node_name="node_name"/>
+    <!--<ComponentMessages :validation_result="validation_result" :node_name="node_name"/>-->
 
     <!-- Component itself -->
     <component
@@ -34,6 +34,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import {set, unset} from 'lodash';
 // import {get, set, unset, cloneDeep} from 'lodash';
 // import {TConfig} from '@locational/config-validation/build/module/lib/config_types/TConfig';
 //
@@ -67,13 +68,13 @@ export default Vue.extend({
 //
 //     validation_result: Object as () => any,
 //   },
-//   data(): Data {
-//     return {
-//       backup_config: null,
-//       show_backup_button: false,
-//       included: true,
-//     };
-//   },
+  data() {
+    return {
+      // backup_config: null,
+      // show_backup_button: false,
+      included: true,
+    };
+  },
 //   computed: {
 //     config(): TConfig {
 //       return this.$store.state.applets_config;
@@ -88,7 +89,7 @@ export default Vue.extend({
 //     this.determine_included();
 //     this.backup_node()
 //   },
-//   methods: {
+  methods: {
 //     handle_included_change() {
 //       if (this.included) {
 //         // show backup button, if we have a backup
@@ -102,22 +103,22 @@ export default Vue.extend({
 //       }
 //       this.save({})
 //     },
-//     save(updated_config: any) {
-//       if (this.included) {
-//         /*
-//           Need to use lodash.set so nested objects get updated.
-//           If not we end up with an object like: { 'applets.irs_record_point': {} }
-//           when we want: { 'applets': {'irs_record_point: {}} }
-//         */
-//         const new_config = {...this.config};
-//         set(new_config, this.path_name, updated_config);
-//         this.$store.commit('set_applets_config', new_config);
-//       } else {/* use unset for same reason as above*/
-//         const new_config = {...this.config};
-//         unset(new_config, this.path_name);
-//         this.$store.commit('set_applets_config', new_config);
-//       }
-//     },
+    save(updated_config: any) {
+      if (this.included) {
+        /*
+          Need to use lodash.set so nested objects get updated.
+          If not we end up with an object like: { 'applets.irs_record_point': {} }
+          when we want: { 'applets': {'irs_record_point: {}} }
+        */
+        const new_config = {...this.config};
+        set(new_config, this.path_name, updated_config);
+        this.$store.commit('set_applets_config', new_config);
+      } else {/* use unset for same reason as above*/
+        const new_config = {...this.config};
+        unset(new_config, this.path_name);
+        this.$store.commit('set_applets_config', new_config);
+      }
+    },
 //     determine_included() {
 //       if (!this.show_include) {
 //         this.included = true;
@@ -138,6 +139,6 @@ export default Vue.extend({
 //       this.show_backup_button = false;
 //       this.save(this.backup_config)
 //     }
-//   },
+  },
 });
 </script>
