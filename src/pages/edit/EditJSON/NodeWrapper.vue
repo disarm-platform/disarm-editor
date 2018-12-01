d18040575dca83e13d61127dd903d657aa3d53c7
Jonathan Smith <jonathan@peoplesized.com>
2018-12-01 12:12:02 +0200

    more lint

--- src/pages/edit/EditJSON/NodeWrapper.vue
+++ src/pages/edit/EditJSON/NodeWrapper.vue
@@ -46,7 +46,7 @@
       };
     },
     computed: {
-      node_config() {
+      node_config():any  {
         return get(this.instance_config, this.path_name);
       },
     },
@@ -54,7 +54,7 @@
       this.node_config_backup = cloneDeep(this.node_config);
     },
     methods: {
-      change_wrapper: debounce(function (this: any, node_config) {
+      change_wrapper: debounce(function(this: any, node_config) {
         const copy = cloneDeep(this.instance_config);
         set(copy, this.path_name, node_config);
         this.$store.commit(CONFIG_MUTATIONS.SET_SELECTED_CONFIG, copy);
