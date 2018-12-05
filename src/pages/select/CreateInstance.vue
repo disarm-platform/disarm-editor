99e55bbe072cbd6fb7f414ec1040d046db69affa
Jonathan Smith <jonathan@peoplesized.com>
2018-12-05 10:09:14 +0200

    don’t clear form while you’re trying to create a new instance

--- src/pages/select/CreateInstance.vue
+++ src/pages/select/CreateInstance.vue
@@ -13,33 +13,36 @@
 </template>
 
 <script lang='ts'>
-import Vue from 'vue';
+  import Vue from 'vue';
+  import {cloneDeep} from 'lodash';
 
-export default Vue.extend({
-  data() {
-    return {
-      ui_visible: false,
-      instance_form: {
-        name: '',
-      },
-    };
-  },
-  computed: {
-    valid(): boolean {
-      return this.instance_form.name !== '';
+  export default Vue.extend({
+    data() {
+      return {
+        ui_visible: false,
+        instance_form: {
+          name: '',
+        },
+      };
     },
-  },
-  methods: {
-    reset_form() {
-      this.instance_form.name = '';
-    },
-    save_instance() {
-      this.$emit('create_new', this.instance_form);
-      this.ui_visible = false;
+    mounted() {
       this.reset_form();
     },
-  },
-});
+    computed: {
+      valid(): boolean {
+        return this.instance_form.name !== '';
+      },
+    },
+    methods: {
+      reset_form() {
+        this.instance_form.name = '';
+      },
+      save_instance() {
+        this.$emit('create_new', cloneDeep(this.instance_form));
+        this.ui_visible = false;
+      },
+    },
+  });
 </script>
 
 <style lang='scss' scoped>
