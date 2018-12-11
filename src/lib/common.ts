4aeec12564461d287680d5b4307be1858c1f12e4
Jonathan Smith <jonathan@peoplesized.com>
2018-12-11 10:01:30 +0200

    better checking and setting of API URL

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -1,7 +1,8 @@
 export default {
   api: {
     version: 'v8',
-    url: 'http://localhost:3000/v8',
+    url: 'https://demo-editor-server.herokuapp.com/v8',
+    query_parameter: 'api-url',
   },
   permissions: {
     dummy_record_value: 'dummy',
